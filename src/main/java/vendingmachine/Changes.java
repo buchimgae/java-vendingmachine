672338abe799ddd7da341b67af7ced3bdccd3ea0
zipshin <egg65536@gmail.com>
2021-05-31 21:08:33 +0900

    ChangesTest @CsvSource 추가

--- src/main/java/vendingmachine/Changes.java
+++ src/main/java/vendingmachine/Changes.java
@@ -4,11 +4,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Changes {
+    private int value;
 
     public Changes(int value) {
-
+        this.value = value;
     }
+
     List<CoinSet> coin(){
-        return Arrays.asList(CoinSet._10_COIN);
+        if(this.value == 10){
+            return Arrays.asList(CoinSet._10_COIN);
+        }
+        if(this.value == 50){
+            return Arrays.asList(CoinSet._50_COIN);
+        }
+        if(this.value == 100){
+            return Arrays.asList(CoinSet._100_COIN);
+        }
+        if(this.value == 500){
+            return Arrays.asList(CoinSet._500_COIN);
+        }
+        return Arrays.asList();
+
     }
 }
--- src/test/java/vendingmachine/ChangesTest.java
+++ src/test/java/vendingmachine/ChangesTest.java
@@ -2,6 +2,9 @@ package vendingmachine;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -10,13 +13,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ChangesTest {
 
-
-    @Test
-    void _10원이_남아_있다면_10원짜리_동전_1개를_돌려준다(){
-        Changes changes = new Changes(10);
+    @ParameterizedTest
+    @CsvSource({"10,_10_COIN","50,_50_COIN","100,_100_COIN","500,_500_COIN"})
+    void n원이_남아_있다면_n원짜리_동전_1개를_돌려준다(int value, CoinSet coin){
+        Changes changes = new Changes(value);
         List<CoinSet> coinSet = changes.coin();
-
-        assertThat(coinSet.size()).isEqualTo(1);
-        assertThat(coinSet.get(0)).isEqualTo(CoinSet._10_COIN);
+        assertThat(coinSet).containsExactlyInAnyOrder(coin);
     }
+
+
+
 }
